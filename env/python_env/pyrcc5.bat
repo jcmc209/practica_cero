97a2907229ee22c7b9320b399e6a87f759e3c0a8
jcmc209 <josecarlsmm@gmail.com>
2024-10-18 18:04:54 +0200

    Nuevo environment

---
+++ env/python_env/pyrcc5.bat
@@ -0,0 +1,1 @@
+@C:/Users/Usuario/Desktop/CUNEF/CIENCIA DE DATOS/APRENDIZAJE AUTOMATICO/practica_cero/env/python_env\python.exe -m PyQt5.pyrcc_main %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ env/python_env/pyuic5.bat
@@ -0,0 +1,1 @@
+@C:/Users/Usuario/Desktop/CUNEF/CIENCIA DE DATOS/APRENDIZAJE AUTOMATICO/practica_cero/env/python_env\python.exe -m PyQt5.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
